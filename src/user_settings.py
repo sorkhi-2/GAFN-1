fb177389fea8794179d7259c0584e472702f5225
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-04-23 15:55:03 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -4,22 +4,23 @@
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
+    "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
     "https://t.me/s/v2rayfree",
     "https://t.me/s/FreeV2rays",
     "https://t.me/s/v2ray_free_conf",
     "https://t.me/s/PrivateVPNs",
     "https://t.me/s/IP_CF_Config",
-    # "https://t.me/s/shadowproxy66",
-    # "https://t.me/s/OutlineReleasedKey",
     "https://t.me/s/prrofile_purple",
-    "https://t.me/s/proxy_shadosocks",
     "https://t.me/s/meli_proxyy",
     "https://t.me/s/DirectVPN",
     "https://t.me/s/VmessProtocol",
-    # "https://t.me/s/ViProxys",
     "https://t.me/s/heyatserver",
     "https://t.me/s/vpnfail_vless",
     "https://t.me/s/DailyV2RY",
+    "https://t.me/s/shadowproxy66",
+    "https://t.me/s/OutlineReleasedKey",
+    "https://t.me/s/proxy_shadosocks",
+    "https://t.me/s/ViProxys",
     # Add more URLs here if you want to include additional sources.
 ]
 
